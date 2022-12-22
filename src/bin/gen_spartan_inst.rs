b5491907905538f172807d936cc6c67f3f5c023d
Daniel Tehrani <contact@dantehrani.com>
2022-12-22 16:19:55 +0100

    Add bin file to compile Circom circuit to spartan circuit

---
+++ src/bin/gen_spartan_inst.rs
@@ -0,0 +1,18 @@
+use serde_json::json;
+use spartan_wasm::circuits::utils::load_as_spartan_inst;
+use std::env::current_dir;
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let root = current_dir().unwrap();
+    let circuit_file = root.join("circuits/build/poseidon/poseidon.r1cs");
+    let spartan_inst = load_as_spartan_inst(circuit_file, 0);
+    let spartan_inst_json = json!(spartan_inst);
+
+    let output_file = root.join("browser_benchmark/public/poseidon_circuit.json");
+    File::create(output_file)
+        .unwrap()
+        .write_all(spartan_inst_json.to_string().as_bytes())
+        .unwrap();
+}
