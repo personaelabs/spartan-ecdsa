b473dca5ad2150c6b93659f2d61f47f26b830ca8
Daniel Tehrani <contact@dantehrani.com>
2023-01-10 17:50:43 +0900

    Use secq256k1 as the prime

--- scripts/membership_circuit.sh
+++ scripts/membership_circuit.sh
@@ -1,2 +1,2 @@
 mkdir -p ./circuits/build/eff_ecdsa_membership
-circom ./circuits/eff_ecdsa_membership/membership.circom --r1cs --wasm --prime curve25519 -o ./circuits/build/ecdsa_membership/
+circom ./circuits/eff_ecdsa_membership/membership.circom --r1cs --wasm --prime secq256k1 -o ./circuits/build/ecdsa_membership/
--- scripts/spartan_poseidon.sh
+++ scripts/spartan_poseidon.sh
@@ -2,6 +2,6 @@ BUILD_DIR=./circuits/build/spartan/poseidon
 CIRCUIT_NAME="poseidon"$1
 
 mkdir -p $BUILD_DIR
-circom ./circuits/poseidon/$CIRCUIT_NAME.circom --r1cs --wasm --prime curve25519 -o $BUILD_DIR
+circom ./circuits/poseidon/$CIRCUIT_NAME.circom --r1cs --wasm --prime secq256k1 -o $BUILD_DIR
 
 cargo run --release --bin gen_spartan_inst -- $BUILD_DIR/$CIRCUIT_NAME.r1cs $BUILD_DIR/$CIRCUIT_NAME.circuit
