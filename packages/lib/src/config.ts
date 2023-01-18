9fa01e7e48b26420d0f7cb6d4da4259c30038666
Daniel Tehrani <contact@dantehrani.com>
2023-01-18 22:17:42 +0900

    Small update

--- packages/lib/src/config.ts
+++ packages/lib/src/config.ts
@@ -4,3 +4,6 @@ export const DEFAULT_EFF_ECDSA_CIRCUIT =
   "https://storage.googleapis.com/proving_keys/eff_ecdsa/eff_ecdsa.circuit";
 export const DEFAULT_SPARTAN_WASM =
   "https://storage.googleapis.com/proving_keys/spartan_wasm.wasm";
+
+export const DEFAULT_EFF_MEMBERSHIP_CIRCUIT = "";
+export const DEFAULT_MEMBERSHIP_WITNESS_GEN_WASM = "";
--- packages/lib/src/core/membership_prover.ts
+++ packages/lib/src/core/membership_prover.ts
@@ -1,5 +1,25 @@
 import { Profiler } from "../helpers/profiler";
+import { IProver, NIZK, ProverOptions } from "../types";
+import { SpartanWasm } from "../wasm";
+import {
+  DEFAULT_EFF_MEMBERSHIP_CIRCUIT,
+  DEFAULT_MEMBERSHIP_WITNESS_GEN_WASM
+} from "../config";
 
-export class MembershipProver extends Profiler {
-  // TBD
+export class MembershipProver extends Profiler implements IProver {
+  spartanWasm: SpartanWasm;
+  circuit: string;
+  witnessGenWasm: string;
+
+  constructor(options?: ProverOptions) {
+    super({ enabled: options?.enableProfiler });
+
+    this.spartanWasm = new SpartanWasm({ spartanWasm: options?.spartanWasm });
+    this.circuit = options?.circuit || DEFAULT_EFF_MEMBERSHIP_CIRCUIT;
+    this.witnessGenWasm =
+      options?.witnessGenWasm || DEFAULT_MEMBERSHIP_WITNESS_GEN_WASM;
+  }
+
+  // @ts-ignore
+  async prove(): Promise<NIZK> {}
 }
