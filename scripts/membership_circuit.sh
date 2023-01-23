2906b7984d7e2f8a5f637e886091a44eea347589
Daniel Tehrani <contact@dantehrani.com>
2023-01-23 20:14:55 +0900

    Small change

--- scripts/membership_circuit.sh
+++ scripts/membership_circuit.sh
@@ -1,4 +1,4 @@
 BUILD_DIR=./packages/circuits/build/membership
 mkdir -p $BUILD_DIR &&
 circom ./packages/circuits/instances/membership.circom --r1cs --wasm --prime secq256k1 -o $BUILD_DIR &&
-cargo run --bin gen_spartan_inst $BUILD_DIR/membership.r1cs $BUILD_DIR/membership.circuit 4
+cargo run --release --bin gen_spartan_inst $BUILD_DIR/membership.r1cs $BUILD_DIR/membership.circuit 4
