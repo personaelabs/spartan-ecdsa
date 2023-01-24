a62c7404e061f5da2fdd363f02371454e76434d4
Daniel Tehrani <contact@dantehrani.com>
2023-01-24 18:57:49 +0900

    Use membership proving in bench/web

--- packages/benchmark/web/pages/index.tsx
+++ packages/benchmark/web/pages/index.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
-import { EffECDSAProver, EffECDSAVerifier } from "spartan-ecdsa";
-import { ecsign, hashPersonalMessage } from "@ethereumjs/util";
+import { MembershipProver, Tree, Poseidon } from "spartan-ecdsa";
+import {
+  ecrecover,
+  ecsign,
+  hashPersonalMessage,
+  privateToPublic
+} from "@ethereumjs/util";
 
 export default function Home() {
   const [proof, setProof] = useState<any | undefined>();
@@ -10,15 +15,42 @@ export default function Home() {
     const msg = Buffer.from("harry potter");
     const msgHash = hashPersonalMessage(msg);
 
-    const { v, r, s } = ecsign(msg, privKey);
+    const { v, r, s } = ecsign(msgHash, privKey);
+    const pubKey = ecrecover(msgHash, v, r, s);
     const sig = `0x${r.toString("hex")}${s.toString("hex")}${v.toString(16)}`;
+
+    const poseidon = new Poseidon();
+    await poseidon.init();
+    const treeDepth = 10;
+    const tree = new Tree(treeDepth, poseidon);
+
+    // Insert prover public key into the tree
+    tree.hashAndInsert(pubKey);
+
+    // Insert other members into the tree
+    for (const member of ["🕵️", "🥷", "👩‍🔬"]) {
+      const pubKey = privateToPublic(
+        Buffer.from("".padStart(16, member), "utf16le")
+      );
+      tree.hashAndInsert(pubKey);
+    }
+
+    const index = tree.indexOf(pubKey);
+    const merkleProof = tree.createProof(index);
 
     console.log("Proving...");
     console.time("Full proving time");
-    const prover = new EffECDSAProver({
+
+    const prover = new MembershipProver(treeDepth, {
       enableProfiler: true
     });
-    const { proof, publicInput } = await prover.prove(sig, msgHash);
+
+    const { proof, publicInput } = await prover.prove(
+      sig,
+      msgHash,
+      merkleProof
+    );
+
     console.timeEnd("Full proving time");
     console.log(
       "Raw proof size (excluding public input)",
@@ -28,6 +60,7 @@ export default function Home() {
     setProof({ proof, publicInput });
   };
 
+  /*
   const verify = async () => {
     if (!proof) {
       console.log("No proof yet!");
@@ -40,11 +73,11 @@ export default function Home() {
       console.log("Verified?", verified);
     }
   };
+  */
 
   return (
     <div>
       <button onClick={prove}>Prove</button>
-      <button onClick={verify}>Verify</button>
     </div>
   );
 }
