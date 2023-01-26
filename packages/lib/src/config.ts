0134d6571ef501ca8c9e701e73cc45a578a9866e
lsankar4033 <lsankar4033@gmail.com>
2023-01-26 09:46:00 -0800

    fix typo

--- packages/lib/src/config.ts
+++ packages/lib/src/config.ts
@@ -1,12 +1,12 @@
 export const DEFAULT_EFF_ECDSA_WITNESS_GEN_WASM =
-  "https://storage.googleapis.com/personae-proving_keys/eff_ecdsa/eff_ecdsa.wasm";
+  "https://storage.googleapis.com/personae-proving-keys/eff_ecdsa/eff_ecdsa.wasm";
 export const DEFAULT_EFF_ECDSA_CIRCUIT =
-  "https://storage.googleapis.com/personae-proving_keys/eff_ecdsa/eff_ecdsa.circuit";
+  "https://storage.googleapis.com/personae-proving-keys/eff_ecdsa/eff_ecdsa.circuit";
 export const DEFAULT_SPARTAN_WASM =
-  "https://storage.googleapis.com/personae-proving_keys/spartan_wasm_bg.wasm";
+  "https://storage.googleapis.com/personae-proving-keys/spartan_wasm_bg.wasm";
 
 export const DEFAULT_MEMBERSHIP_WITNESS_GEN_WASM =
   "https://storage.googleapis.com/personae-proving-keys/spartan_wasm_bg.wasm";
 
 export const DEFAULT_MEMBERSHIP_CIRCUIT =
-  "https://storage.googleapis.com/personae-proving_keys/membership/membership.circuit";
+  "https://storage.googleapis.com/personae-proving-keys/membership/membership.circuit";
