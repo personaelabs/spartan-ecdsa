a22a8aaf735984688c5d0e2fcf739fee672b82c8
lsankar4033 <lsankar4033@gmail.com>
2023-01-26 20:55:18 -0800

    another typo fix

--- packages/lib/src/config.ts
+++ packages/lib/src/config.ts
@@ -6,7 +6,7 @@ export const DEFAULT_SPARTAN_WASM =
   "https://storage.googleapis.com/personae-proving-keys/spartan_wasm_bg.wasm";
 
 export const DEFAULT_MEMBERSHIP_WITNESS_GEN_WASM =
-  "https://storage.googleapis.com/personae-proving-keys/spartan_wasm_bg.wasm";
+  "https://storage.googleapis.com/personae-proving-keys/membership/membership.wasm";
 
 export const DEFAULT_MEMBERSHIP_CIRCUIT =
   "https://storage.googleapis.com/personae-proving-keys/membership/membership.circuit";
