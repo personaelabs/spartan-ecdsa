a290a1936b8c8dc783cc7b3a698cd60fccfb6b6b
Daniel Tehrani <contact@dantehrani.com>
2023-01-29 13:32:58 +0900

    Fix poseidon parm generation

--- packages/poseidon/k256_params.sh
+++ packages/poseidon/k256_params.sh
@@ -1,1 +1,1 @@
-sage generate_params_poseidon.sage 1 0 256 3 5 128 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+sage ./sage/generate_params_poseidon.sage 1 0 256 3 5 128 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
