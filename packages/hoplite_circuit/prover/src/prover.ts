69a7821880c534105369e90cf20ce4508251febd
Daniel Tehrani <contact@dantehrani.com>
2023-03-08 11:35:19 -0800

    Add a prover to generate proofs which is used for testing the spartan-ecdsa halo2 circuit

---
+++ packages/hoplite_circuit/prover/src/prover.ts
@@ -0,0 +1,84 @@
+import * as fs from "fs";
+import {
+  MembershipProver,
+  Poseidon,
+  Tree,
+  MembershipVerifier
+} from "@personaelabs/spartan-ecdsa";
+import {
+  hashPersonalMessage,
+  ecsign,
+  ecrecover,
+  privateToPublic
+} from "@ethereumjs/util";
+import * as path from "path";
+
+const prove = async () => {
+  const privKey = Buffer.from("".padStart(16, "🧙"), "utf16le");
+  const msg = Buffer.from("harry potter");
+  const msgHash = hashPersonalMessage(msg);
+
+  const { v, r, s } = ecsign(msgHash, privKey);
+  const pubKey = ecrecover(msgHash, v, r, s);
+  const sig = `0x${r.toString("hex")}${s.toString("hex")}${v.toString(16)}`;
+
+  // Init the Poseidon hash
+  const poseidon = new Poseidon();
+  await poseidon.initWasm();
+
+  const treeDepth = 20;
+  const tree = new Tree(treeDepth, poseidon);
+
+  // Get the prover public key hash
+  const proverPubkeyHash = poseidon.hashPubKey(pubKey);
+
+  // Insert prover public key hash into the tree
+  tree.insert(proverPubkeyHash);
+
+  // Insert other members into the tree
+  for (const member of ["🕵️", "🥷", "👩‍🔬"]) {
+    const pubKey = privateToPublic(
+      Buffer.from("".padStart(16, member), "utf16le")
+    );
+    tree.insert(poseidon.hashPubKey(pubKey));
+  }
+
+  // Compute the merkle proof
+  const index = tree.indexOf(proverPubkeyHash);
+  const merkleProof = tree.createProof(index);
+
+  const proverConfig = {
+    circuit: path.join(
+      __dirname,
+      "../../../circuits/build/pubkey_membership/pubkey_membership.circuit"
+    ),
+    witnessGenWasm: path.join(
+      __dirname,
+      "../../../circuits/build/pubkey_membership/pubkey_membership_js/pubkey_membership.wasm"
+    ),
+    enableProfiler: true
+  };
+
+  // Init the prover
+  const prover = new MembershipProver(proverConfig);
+  await prover.initWasm();
+
+  // Prove membership
+  const { proof, publicInput } = await prover.prove(sig, msgHash, merkleProof);
+  fs.writeFileSync("./proof.bin", proof);
+  fs.writeFileSync("./input.bin", publicInput.serialize());
+
+  const verifierConfig = {
+    circuit: proverConfig.circuit,
+    enableProfiler: true
+  };
+
+  // Init verifier
+  const verifier = new MembershipVerifier(verifierConfig);
+  await verifier.initWasm();
+
+  // Verify proof
+  await verifier.verify(proof, publicInput.serialize());
+};
+
+prove();
