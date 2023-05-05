4b8acc430d8bcb9ae8bd85ea5c5b9c83a209fad3
Daniel Tehrani <contact@dantehrani.com>
2023-05-05 12:49:40 +0200

    feat: use lazy_static to load the constants

--- packages/poseidon/src/poseidon_k256.rs
+++ packages/poseidon/src/poseidon_k256.rs
@@ -1,28 +1,18 @@
-use crate::{k256_consts, Poseidon, PoseidonConstants};
-use ff::PrimeField;
+use crate::k256_consts::*;
+use crate::{Poseidon, PoseidonConstants};
 pub use secq256k1::field::field_secp::FieldElement;
 
 #[allow(dead_code)]
 pub fn hash(input: &[FieldElement; 2]) -> FieldElement {
-    let round_constants: Vec<FieldElement> = k256_consts::ROUND_CONSTANTS
-        .iter()
-        .map(|x| FieldElement::from_str_vartime(x).unwrap())
-        .collect();
-
-    let mds_matrix: Vec<Vec<FieldElement>> = k256_consts::MDS_MATRIX
-        .iter()
-        .map(|x| {
-            x.iter()
-                .map(|y| FieldElement::from_str_vartime(y).unwrap())
-                .collect::<Vec<FieldElement>>()
-        })
-        .collect();
-
     let constants = PoseidonConstants::<FieldElement>::new(
-        round_constants,
-        mds_matrix,
-        k256_consts::NUM_FULL_ROUNDS,
-        k256_consts::NUM_PARTIAL_ROUNDS,
+        ROUND_CONSTANTS.to_vec(),
+        vec![
+            MDS_MATRIX[0].to_vec(),
+            MDS_MATRIX[1].to_vec(),
+            MDS_MATRIX[2].to_vec(),
+        ],
+        NUM_FULL_ROUNDS,
+        NUM_PARTIAL_ROUNDS,
     );
     let mut poseidon = Poseidon::new(constants);
 
