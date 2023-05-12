adb73907fd0e4dde1e2b9f8034f009b14f469245
Daniel Tehrani <contact@dantehrani.com>
2023-05-12 13:34:38 +0200

    Fix WebAssembly instantiation

--- packages/lib/src/wasm/wasm.js
+++ packages/lib/src/wasm/wasm.js
@@ -399,18 +399,23 @@ function finalizeInit(instance, module) {
     return wasm;
 }
 
-function initSync(module, maybe_memory) {
-    const imports = getImports();
+async function initSync(module, maybe_memory) {
+    if (!wasm) {
+        const imports = getImports();
 
-    initMemory(imports, maybe_memory);
-
-    if (!(module instanceof WebAssembly.Module)) {
-        module = new WebAssembly.Module(module);
+        initMemory(imports, maybe_memory);
+    
+        /*
+        if (!(module instanceof WebAssembly.Module)) {
+            module = new WebAssembly.Module(module);
+        }
+        */
+       const compiled = WebAssembly.compile(module);
+    
+        const instance = await WebAssembly.instantiate(await compiled, imports);
+    
+        return finalizeInit(instance, module);
     }
-
-    const instance = new WebAssembly.Instance(module, imports);
-
-    return finalizeInit(instance, module);
 }
 
 async function init(input, maybe_memory) {
